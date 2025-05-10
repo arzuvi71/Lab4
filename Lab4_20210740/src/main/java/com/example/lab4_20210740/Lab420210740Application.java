b804adf64b343eca375b5b7d53061e1128f26cef
Labtel <a20210740@pucp.edu.pe>
2025-05-09 20:41:31 -0500

    Commit inicial

---
+++ Lab4_20210740/src/main/java/com/example/lab4_20210740/Lab420210740Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20210740;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420210740Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420210740Application.class, args);
+    }
+
+}
